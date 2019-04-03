0eb9c94a361b7838b7d2909f35b58e20dd2910ef
MarcusDragsten <44369684+MarcusDragsten@users.noreply.github.com>
2019-04-03 15:54:05 +0200

    Nesten ferdig med rapportsiden

--- services/bestillingService.js
+++ services/bestillingService.js
@@ -87,9 +87,19 @@ class BestillingService extends Component {
     );
   }
 
-  hentRapport(success) {
+  hentTotalRapport(success) {
     connection.query(
       'SELECT Bestilling.id, bestilling_type, utlevering_dato, utlevering_tid, faktiskInnlevering_dato, faktiskInnlevering_tid, epost, fornavn, etternavn, gevinst FROM Bestilling INNER JOIN Kunder ON Bestilling.kunde_epost = Kunder.epost WHERE faktiskInnlevering_dato IS NOT NULL AND faktiskInnlevering_tid IS NOT NULL ORDER BY id',
+      (error, results) => {
+        if (error) return console.error(error);
+        success(results);
+      }
+    );
+  }
+
+  hentAnsatteRapport(success) {
+    connection.query(
+      'SELECT Bestilling.id, bestilling_type, utlevering_dato, utlevering_tid, faktiskInnlevering_dato, faktiskInnlevering_tid, Kunder.epost, Ansatte.fornavn, Ansatte.etternavn, Ansatte.telefon, gevinst FROM Bestilling INNER JOIN Kunder ON Bestilling.kunde_epost = Kunder.epost INNER JOIN Ansatte ON Bestilling.selger = Ansatte.id WHERE faktiskInnlevering_dato IS NOT NULL AND faktiskInnlevering_tid IS NOT NULL ORDER BY id',
       (error, results) => {
         if (error) return console.error(error);
         success(results);
@@ -183,6 +193,19 @@ class BestillingService extends Component {
       }
     );
   }
+
+  sok4(utlevering_dato, faktiskInnlevering_dato, ansatteId, success) {
+    connection.query(
+      'SELECT Bestilling.id, bestilling_type, utlevering_dato, utlevering_tid, faktiskInnlevering_dato, faktiskInnlevering_tid, Kunder.epost, Ansatte.fornavn, Ansatte.etternavn, Ansatte.telefon, gevinst FROM Bestilling INNER JOIN Kunder ON Bestilling.kunde_epost = Kunder.epost INNER JOIN Ansatte on Bestilling.selger = Ansatte.id WHERE utlevering_dato >= ? AND faktiskInnlevering_dato <= ? AND Ansatte.id = ? AND faktiskInnlevering_tid IS NOT NULL AND faktiskInnlevering_dato IS NOT NULL',
+      [utlevering_dato, faktiskInnlevering_dato, ansatteId],
+      (error, results) => {
+        if (error) return console.error(error);
+
+        success(results);
+      }
+    );
+  }
+
   updateInnlevering(faktiskInnlevering_dato, faktiskInnlevering_tid, id, success) {
     connection.query(
       'update Bestilling set faktiskInnlevering_dato=? , faktiskInnlevering_tid=? where id=?',
